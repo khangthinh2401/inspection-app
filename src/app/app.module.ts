e537ab2216c5c2372796c40dcdc9aecf97a8ec77
khangthinh2401 <khangthinh2401@gmail.com>
2022-08-04 13:40:42 +0700

    Add modules to app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { InspectionComponent } from './inspection/inspection.component';
 import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
 import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
 
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { InspectionApiService } from "./inspection-api.service";
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    InspectionComponent,
-    ShowInspectionComponent,
-    AddEditInspectionComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        InspectionComponent,
+        ShowInspectionComponent,
+        AddEditInspectionComponent
+    ],
+    imports: [
+        BrowserModule,
+        // add modules
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [InspectionApiService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
