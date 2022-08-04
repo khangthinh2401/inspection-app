5e3dbb3fe0d3080102151184d5003f0ec8e9d8cf
khangthinh2401 <khangthinh2401@gmail.com>
2022-08-04 15:24:41 +0700

    Finish show inspection

--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { InspectionApiService } from "../../inspection-api.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-show-inspection',
-  templateUrl: './show-inspection.component.html',
-  styleUrls: ['./show-inspection.component.css']
+    selector: 'app-show-inspection',
+    templateUrl: './show-inspection.component.html',
+    styleUrls: ['./show-inspection.component.css']
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+    inspectionList$!: Observable<any[]>;
+    inspectionTypesList$!: Observable<any[]>;
+    inspectionTypesList: any = [];
 
-  ngOnInit(): void {
-  }
+    // map to display data associated with foreign keys
+    inspectionTypesMap: Map<number, string> = new Map();
+
+    // call api service
+    constructor(private service: InspectionApiService) { }
+
+    ngOnInit(): void {
+        // call getInspectionList() function of service
+        // assign returned data into inspectionList$
+        this.inspectionList$ = this.service.getInspectionList();
+    }
 
 }
