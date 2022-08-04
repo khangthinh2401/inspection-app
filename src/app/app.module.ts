461249040d9ed8c8971c1e4923135daa66b0ff89
khangthinh2401 <khangthinh2401@gmail.com>
2022-08-04 13:34:36 +0700

    Generate show-inspection and add-edit-inspection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
+import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InspectionComponent
+    InspectionComponent,
+    ShowInspectionComponent,
+    AddEditInspectionComponent
   ],
   imports: [
     BrowserModule
