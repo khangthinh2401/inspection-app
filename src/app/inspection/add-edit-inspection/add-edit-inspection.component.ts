54b7cc40f7098a852346a264f543551d79978061
khangthinh2401 <khangthinh2401@gmail.com>
2022-08-04 23:21:46 +0700

    finish form

--- src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
+++ src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { InspectionApiService } from "../../inspection-api.service";
 
 @Component({
-  selector: 'app-add-edit-inspection',
-  templateUrl: './add-edit-inspection.component.html',
-  styleUrls: ['./add-edit-inspection.component.css']
+    selector: 'app-add-edit-inspection',
+    templateUrl: './add-edit-inspection.component.html',
+    styleUrls: ['./add-edit-inspection.component.css']
 })
 export class AddEditInspectionComponent implements OnInit {
 
-  constructor() { }
+    inspectionList$!: Observable<any[]>;
+    statusList$!: Observable<any[]>;
+    inspectionTypesList$!: Observable<any[]>;
 
-  ngOnInit(): void {
-  }
+    constructor(private service: InspectionApiService) { }
 
+    ngOnInit(): void {
+        this.id = this.inspection.id;
+        this.status = this.inspection.status;
+        this.comments = this.inspection.comments;
+        this.inspectionTypeId = this.inspection.inspectionTypeId;
+
+        this.statusList$ = this.service.getStatusList();
+        this.inspectionList$ = this.service.getInspectionList();
+        this.inspectionTypesList$ = this.service.getInspectionTypesList();
+    }
+
+    @Input() inspection: any;
+    id: number = 0;
+    status: string = "";
+    comments: string = "";
+    inspectionTypeId!: number;
+
+    addInspection() {
+        var inspection = {
+            inspectionTypeId: this.inspectionTypeId,
+            status: this.status,
+            comments: this.comments
+        }
+        this.service.addInspection(inspection).subscribe(res => {
+            var closeModalBtn = document.getElementById('add-edit-modal-close');
+            if (closeModalBtn) {
+                closeModalBtn.click();
+            }
+
+            var showAddSuccess = document.getElementById('add-success-alert');
+            if (showAddSuccess) {
+                showAddSuccess.style.display = "block";
+            }
+            setTimeout(function () {
+                if (showAddSuccess) {
+                    showAddSuccess.style.display = "none";
+                }
+            }, 4000)
+        })
+    }
+    updateInspection() {
+
+    }
 }
