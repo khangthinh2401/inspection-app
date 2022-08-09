6979b2e6116e4885f0f18ec4bbf818fde6ef82ea
khangthinh2401 <khangthinh2401@gmail.com>
2022-08-09 15:28:35 +0700

    Update url

--- src/app/inspection-api.service.ts
+++ src/app/inspection-api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class InspectionApiService {
 
-    readonly inspectionAPIUrl = "https://localhost:5000/api";
+    readonly inspectionAPIUrl = "http://103.195.237.73:8080/api";
 
     constructor(private http: HttpClient) { }
 
